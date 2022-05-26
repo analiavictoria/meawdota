46e98ab4b8352bae4dc70d59dfd756afdb4d330d
Analia <analiavictoire@gmail.com>
2022-05-25 21:51:53 -0400

    Front quase final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { AdoteComponent } from './pages/adote/adote.component';
+import { ApoieComponent } from './pages/apoie/apoie.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SobrenosComponent } from './pages/sobrenos/sobrenos.component';
+import { CardsComponent } from './pages/adote/cards/cards.component';
+import { ConhecerComponent } from './pages/adote/conhecer/conhecer.component';
+
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'adote', component: AdoteComponent},
+  {path: 'apoie', component: ApoieComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'sobrenos', component: SobrenosComponent},
+  {path: 'cards', component: CardsComponent},
+  {path: 'conhecer', component:ConhecerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,64 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/home/header/header.component';
+import { NavComponent } from './pages/adote/nav/nav.component';
+import { CardsComponent } from './pages/adote/cards/cards.component';
+import { SobrenosComponent } from './pages/sobrenos/sobrenos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { ApoieComponent } from './pages/apoie/apoie.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AdoteComponent } from './pages/adote/adote.component';
+import { FooterComponent } from './pages/home/footer/footer.component';
+import { ConhecerComponent } from './pages/adote/conhecer/conhecer.component';
+
+//Material imports
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatCardModule} from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule} from '@angular/material/icon';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatSelectModule} from '@angular/material/select';
+import { MatPaginatorModule} from '@angular/material/paginator';
+import { MatChipsModule} from '@angular/material/chips';
+import { MatListModule} from '@angular/material/list';
+
+import { FormsModule, ReactiveFormsModule  } from "@angular/forms";
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    CardsComponent,
+    SobrenosComponent,
+    HomeComponent,
+    ApoieComponent,
+    LoginComponent,
+    AdoteComponent,
+    FooterComponent,
+    ConhecerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatPaginatorModule,
+    MatChipsModule,
+    MatListModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
